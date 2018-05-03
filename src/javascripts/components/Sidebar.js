af41e898e78a37a9c2acd5d7ab2b87df02337440
Harry Uglow <hu115@ic.ac.uk>
2018-05-03 18:00:17 +0100

    Added ant design framework.

--- src/javascripts/components/Sidebar.js
+++ src/javascripts/components/Sidebar.js
@@ -1,1 +1,15 @@
+import React, { Component } from "react";
 
+class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Committee</h2>
+        <p>Committee info coming soon
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
