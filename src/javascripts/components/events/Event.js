1674b898048f1719933dcbec28416f50cfc1e5ba
Harry Uglow <hu115@ic.ac.uk>
2018-08-27 21:42:58 +0100

    Proper state management in Events.

--- src/javascripts/components/events/Event.js
+++ src/javascripts/components/events/Event.js
@@ -3,43 +3,23 @@ import { Col, Image, Row, Well } from "react-bootstrap";
 import fresh from "../../../images/fresh.jpg";
 
 import '../../../stylesheets/Events.css';
-import { gql } from "apollo-boost/lib/index";
-import { Query } from "react-apollo/react-apollo.browser.umd";
 
-const Event = ({ id }) => (
-  <Query
-    query={gql`query Event($id: String!) {
-      events(id: $id) @client {
-        title
-        description
-        date
-        where
-      }
-    }`}
-    variables={{ id }}
-  >
-    {({ loading, error, data: { events } }) =>
-      loading ? <p>Loading...</p>
-        : error ? <p>Error :(</p>
-        : events.map(({ title, description, date, where }) => (
-          <Col sm={6} className="event-col">
-            <Well className="event">
-              <p className="event-title">{title}</p>
-              <Row className="event-details">
-                <Col md={6} mdPush={6} className="well-image">
-                  <Image src={fresh} rounded alt="Freshers Fair"/>
-                </Col>
-                <Col md={6} mdPull={6} className="event-details-text">
-                  <p className="event-description">{description}</p>
-                  <p className="event-info"><strong>Date: </strong>{date}</p>
-                  <p className="event-info"><strong>Where: </strong>{where}</p>
-                </Col>
-              </Row>
-            </Well>
-          </Col>
-        ))
-    }
-  </Query>
+const Event = ({ event: { id, title, description, date, where } }) => (
+  <Col sm={6} className="event-col">
+    <Well className="event">
+      <p className="event-title">{title}</p>
+      <Row className="event-details">
+        <Col md={6} mdPush={6} className="well-image">
+          <Image src={fresh} rounded alt="Freshers Fair"/>
+        </Col>
+        <Col md={6} mdPull={6} className="event-details-text">
+          <p className="event-description">{description}</p>
+          <p className="event-info"><strong>Date: </strong>{date}</p>
+          <p className="event-info"><strong>Where: </strong>{where}</p>
+        </Col>
+      </Row>
+    </Well>
+  </Col>
 );
 
 export default Event;
--- src/javascripts/components/events/Events.js
+++ src/javascripts/components/events/Events.js
@@ -1,36 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 import { Col, Grid, Row } from "react-bootstrap";
 
 import Event from "./Event";
+import { gql } from "apollo-boost/lib/index";
+import { Query } from "react-apollo/react-apollo.browser.umd";
 
-const events = ["freshFair18", "off18", "firstCSP18", "VGBs18"];
-
-class Events extends Component {
-  render() {
-    let eventPairs = [];
-    events.forEach((event, index) => (index % 2 === 0)
-      ? eventPairs.push([event, events[index + 1]])
-      : null
-    );
-    return (
-      <div className="content">
-        <Grid className="thin">
-          <Row>
-            <Col smHidden={true} mdHidden={true} lgHidden={true} className="event-header" xs={12}>
-              <p className="event-title">Upcoming Events</p>
-              <hr className="separator no-top" />
-            </Col>
-          </Row>
-          {eventPairs.map(eventPair => (
-            <Row className="event-row">
-              <Event id={eventPair[0]}/>
-              {eventPair[1] ? <Event id={eventPair[1]}/> : <div/>}
-            </Row>
-          ))}
-        </Grid>
-      </div>
-    );
-  }
-}
+const Events = () => (
+  <div className="content">
+    <Grid className="thin">
+      <Row>
+        <Col smHidden={true} mdHidden={true} lgHidden={true} className="event-header" xs={12}>
+          <p className="event-title">Upcoming Events</p>
+          <hr className="separator no-top"/>
+        </Col>
+      </Row>
+      <Query query={gql`{
+        events @client {
+          id
+          title
+          description
+          date
+          where
+        }
+      }`}>
+        {({ loading, error, data: { events } }) =>
+          loading ? <p>Loading...</p>
+            : error ? <p>Error :(</p>
+            : (<Row className="event-row">
+                {events.map(event => <Event event={event}/>)}
+              </Row>
+            )
+        }
+      </Query>
+    </Grid>
+  </div>
+);
 
 export default Events;
